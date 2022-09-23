81d04f2aa61d88b836c51b8f42b39330da56a96f
endiesworld <endiesworld@gmail.com>
2022-09-23 02:29:57 +0100

    codacy update

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
-const express = require('express')
-const rateLimit = require("express-rate-limit")
+const express = require('express') ;
+const rateLimit = require("express-rate-limit") ;
 
-
-const min_time = 0
+const hostname = 'localhost';
+const port = 3000;
+const min_time = 0 ;
 const today = Date.now();
 
-const app = express()
+const app = express() ;
 
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 15 minutes
-	max: 3, // Limit each IP to 3 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: true, // Disable the `X-RateLimit-*` headers
-})
+	windowMs: 1 * 60 * 1000, 
+	max: 3, 
+	standardHeaders: true, 
+	legacyHeaders: true, 
+}) ;
 
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter) ;
 
-app.get('/howold', (req, res, next)=>{
-    const {dob} = req.query
+app.get('/howold', (req, res)=>{
+    const {dob} = req.query ;
     if (dob && (dob > min_time) && ((dob * 1000) < today)) {
-        const myDate = new Date(dob * 1000)
-        const daysDiff = Math.round((today - myDate) / (1000 * 60 * 60  * 24))
-        const timestamp = dob
-        const year = myDate.getFullYear()
-        const month = myDate.getUTCMonth() + 1
-        const day = myDate.getDate()
-        const age = Math.floor(daysDiff / 356)
+        const myDate = new Date(dob * 1000) ;
+        const daysDiff = Math.round((today - myDate) / (1000 * 60 * 60  * 24)) ;
+        const timestamp = dob ;
+        const year = myDate.getFullYear() ;
+        const month = myDate.getUTCMonth() + 1 ;
+        const day = myDate.getDate() ;
+        const age = Math.floor(daysDiff / 356) ;
 
         const content = {timestamp,  year,
-                month, day, age}
+                month, day, age} ;
         
-        res.status(200).json(content)
+        res.status(200).json(content) ;
     }
     else{
 
-        res.status(400).json({'error': 'Invalid dob value'})
+        res.status(400).json({'error': 'Invalid dob value'}) ;
     }
     
 })
 
-app.listen(3000)
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 
